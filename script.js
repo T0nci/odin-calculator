e946a39de05c4e0acdb6774390f634875f494f04
T0nci <138928016+T0nci@users.noreply.github.com>
2024-01-15 22:31:47 +0100

    feat: Display the new numbers upon clicking button
    Added Event Listeners to each button to either update the display or
    reassign the number in the display based on if the number is
    an answer/the number is a 0 or alert a message when trying to break
    the number limit.
    
    Also upon each button click, we store the value in a variable so we
    can give it to a number variable for operation.

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let num1 = 0;
 let operator = "";
 let num2 = 0;
 
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+let numIsAnswer = false; // Variable which keeps the answer immutable
+let displayValue = ""; // Variable used to store temporary numbers
+
+numbers.forEach(button => button.addEventListener("click", populateDisplay));
+
 
 function add(a, b) {
     return a + b;
@@ -35,5 +44,17 @@ function operate(a, b, operator) {
             return divide(a, b);
         default:
             return 0;
+    }
+}
+
+
+function populateDisplay(event) {
+    number = event.target.textContent;
+    if (display.textContent === "0" || numIsAnswer) {
+        displayValue = (display.textContent = number);
+    } else if (display.textContent.length === 9) {
+        alert("Max size reached");
+    } else {
+        displayValue = (display.textContent += number);
     }
 }
